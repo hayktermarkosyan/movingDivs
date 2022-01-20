721bf1d8ee980b31be61ea5cae1d7777aab78358
hayktermarkosyan <hayktermarkosyan@gmail.com>
2022-01-20 15:21:08 +0400

    20.01.22

--- script.js
+++ script.js
@@ -23,31 +23,48 @@ yBox.style.height = heightRange1.value + 'px';
 
 
 widthRange1.oninput = function() {
-    if((xBox.offsetLeft - 10) >= (this.value - 100) / 2) {
-        xBox.style.left = (10 + (this.value - 100) / 2) + 'px';
-        xBox.style.width = this.value + 'px';
-    } else {
-        xBox.style.width = this.value + 'px';
-    }    
+    let x1 = xBox.offsetLeft + xBox.offsetWidth /2;
+    let x2 = xBox.offsetLeft + this.valueAsNumber / 2;
+    let dx = x1 - x2; // dx = xBox.offsetWidth /2 - this.valueAsNumber / 2
+
+    if(dx < 0 && (xBox.offsetLeft + dx < frame.offsetLeft    // check left border
+            || xBox.offsetLeft + dx + this.valueAsNumber > frame.offsetLeft + frame.offsetWidth)    // check right border
+        ){
+        return
+    }
+
+    xBox.style.left = xBox.offsetLeft + dx + 'px';
+    xBox.style.width = this.value + 'px';
 }
 
-console.log(xBox.offsetLeft)
-
-// ++++++++++++++++++++++++++++++++
 heightRange1.oninput = function() {
-    xBox.style.top = (155 - (this.value - 100) / 2) + 'px';
+    xBox.style.top = 155 - (this.valueAsNumber - 100) / 2 + 'px';
     xBox.style.height = this.value + 'px';
 }
 
-// ++++++++++++++++++++++++++++++++
 widthRange2.oninput = function() {
-    yBox.style.left = (408 - (this.value - 100) / 2) + 'px';
+    yBox.style.left = 408 - (this.valueAsNumber - 100) / 2 + 'px';
     yBox.style.width = this.value + 'px';
 }
 
 heightRange2.oninput = function() {
+    let y1 = yBox.offsetTop + yBox.offsetHeight / 2;
+    let y2 = yBox.offsetTop + this.valueAsNumber / 2;
+    let dy = y1 - y2;
+
+    let heightDifference = yBox.offsetHeight - this.valueAsNumber;
+
+    if(dy < 0 && (yBox.offsetTop + dy < frame.offsetTop    // check top border
+            || yBox.offsetTop + dy + this.valueAsNumber > frame.offsetTop + frame.offsetHeight)    // check bottom border
+            ){
+        return
+    }
+
+    yBox.style.top = yBox.offsetTop + dy + 'px';
     yBox.style.height = this.value + 'px';
-    yBox.style.top = (((yBox.offsetTop + 61) + this.value / 2) / 2) + 'px';
+    
+    xBox.style.top = (xBox.offsetTop - (this.valueAsNumber + frame.offsetTop) 
+                        + heightDifference) + 'px';
 }
 
 let frameWidth = frame.offsetWidth;
@@ -67,13 +84,13 @@ document.querySelectorAll('.yBox-sets .color button').forEach(function(btn) {
 
 
 
-
 speedRange1.oninput = function() {
-    deltaLeft = this.value;
+    deltaLeft = deltaLeft < 0 ? - this.valueAsNumber : this.valueAsNumber; 
+    // deltaLeft = deltaLeft < 0 ? - parseInt(this.value) : parseInt(this.value); same
 }
 
 speedRange2.oninput = function() {
-    deltaTop = this.value;
+    deltaTop = deltaTop < 0 ? - this.valueAsNumber : this.valueAsNumber;
 }
 
 
@@ -109,7 +126,28 @@ setInterval(() => {
     }
     yBox.style.top = boxTop + 'px';
 
-}, 1000)
+    // boxes X and Y coordinates
+
+    let xBoxRightSideTopCoordinateX = xBox.offsetLeft + xBox.offsetWidth;
+    let xBoxLeftSideTopCoordinateX = xBox.offsetLeft;
+    let xBoxLeftSideTopCoordinateY = xBox.offsetTop;
+    let xBoxLeftSideBottomCoordinateY = xBox.offsetTop + xBox.offsetHeight;
+
+    let yBoxRightSideTopCoordinateX = yBox.offsetLeft + yBox.offsetWidth;
+    let yBoxLeftSideTopCoordinateX = yBox.offsetLeft;
+    let yBoxLeftSideTopCoordinateY = yBox.offsetTop;
+    let yBoxLeftSideBottomCoordinateY = yBox.offsetTop + yBox.offsetHeight;
+
+    // opacity when cross
+    if ((xBoxRightSideTopCoordinateX >= yBoxLeftSideTopCoordinateX && 
+        xBoxLeftSideTopCoordinateX <= yBoxRightSideTopCoordinateX) && 
+        (yBoxLeftSideBottomCoordinateY >= xBoxLeftSideTopCoordinateY &&
+            yBoxLeftSideTopCoordinateY <= xBoxLeftSideBottomCoordinateY)) {
+        xBox.style.opacity = 0.5;
+    } else {
+        xBox.style.opacity = 1;
+    }
+}, 5)
 
 
 window.addEventListener('resize', () => {
